112e50ca7f2196835bb23b35cde049d35b895bb2
Karan Chilamwar <karan.chilamwar@roche.com>
2023-04-21 18:30:59 +0530

    for test

--- src/test/java/service/ContractEmployeeServiceTest4.java
+++ src/test/java/service/ContractEmployeeServiceTest4.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collections;
@@ -17,7 +15,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-public class ContractEmployeeServiceTest {
+public class ContractEmployeeServiceTest4 {
 
 
     @Mock
